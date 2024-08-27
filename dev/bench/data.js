f31690c0312e6a0ba45674d06a2387341b0a3d33
github-action-benchmark <github@users.noreply.github.com>
2024-08-27 13:34:38 +0000

    add Benchmark (go) benchmark result for 1af0b5b486eb526f6723c4eaa096813902b85714

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1724765678320,
+  "repoUrl": "https://github.com/olomix/go-exp",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "oleg.lomaka@gmail.com",
+            "name": "Oleg Lomaka",
+            "username": "olomix"
+          },
+          "committer": {
+            "email": "oleg.lomaka@gmail.com",
+            "name": "Oleg Lomaka",
+            "username": "olomix"
+          },
+          "distinct": true,
+          "id": "1af0b5b486eb526f6723c4eaa096813902b85714",
+          "message": "setup permissions to push to gh-pages branch",
+          "timestamp": "2024-08-27T09:34:09-04:00",
+          "tree_id": "0180fc4392f372ebbc39c4033edc969785934766",
+          "url": "https://github.com/olomix/go-exp/commit/1af0b5b486eb526f6723c4eaa096813902b85714"
+        },
+        "date": 1724765678055,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkOne",
+            "value": 100229418,
+            "unit": "ns/op",
+            "extra": "10 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkTwo",
+            "value": 200386300,
+            "unit": "ns/op",
+            "extra": "5 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
